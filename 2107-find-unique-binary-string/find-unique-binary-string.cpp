2d5bb1218f4c5dfbc5593a6aa375cbf006e0e43f
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-02-20 10:18:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (69.88%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int binaryToDecimal(const string& s) {
+        int dec_value = 0;
+        int base = 1; 
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '1') {
+                dec_value += base;
+            }
+            base *= 2;
+        }
+
+        return dec_value;
+    }
+
+    string decToBinary(int n, int length) {
+        string bin = "";
+        while (n > 0) {
+            bin.push_back('0' + (n % 2));
+            n /= 2;
+        }
+        while (bin.size() < length) { 
+            bin.push_back('0');
+        }
+        reverse(bin.begin(), bin.end());
+        return bin;
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        set<int> st;
+
+        for (auto& num : nums) {
+            st.insert(binaryToDecimal(num));
+        }
+
+        for (int i = 0; i < (1 << n); i++) { 
+            if (st.find(i) == st.end()) {
+                return decToBinary(i, n);
+            }
+        }
+
+        return "";
+    }
+};
