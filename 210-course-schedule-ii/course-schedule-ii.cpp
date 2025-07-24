b71108177b9dde15805475b4908881ef6f845e5b
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-07-24 20:50:56 +0530

    Time: 1 ms (83.05%) | Memory: 17.9 MB (89.93%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(n,vector<int>());
+
+        for(vector<int> &a : prerequisites){
+            graph[a[0]].push_back(a[1]);
+        }
+
+        queue<int> q;
+        vector<int> ind(n,0);
+        for(int i=0;i<n;i++){
+            for(auto &a : graph[i]){
+                ind[a]++;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(ind[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int a = q.front();q.pop();
+            ans.push_back(a);
+            for(int i : graph[a]){
+                if(--ind[i] == 0){
+                    q.push(i);
+                }
+            }
+        }
+
+        reverse(ans.begin(),ans.end());
+        return ans.size() == n ? ans : vector<int>{}; 
+    }
+};
