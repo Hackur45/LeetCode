e7f3ac9331d661090bb80e961fd11378a0ce0c13
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-07-25 09:29:47 +0530

    Time: 8 ms (26.99%) | Memory: 23.6 MB (94.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -10,16 +10,11 @@ public:
             int start = intervals[i][0];
             int end = intervals[i][1];
 
-            if(!ans.empty() && ans.back()[1] >= end) continue;
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0] <= end){
-                    end = max(end,intervals[j][1]);
-                }else{
-                    break;
-                }
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-
-            ans.push_back({start,end});
         }
 
         return ans;
