bff8226bd57d8373907a477dc27ad4e68d16e08b
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-08-12 20:51:52 +0530

    Time: 3 ms (6.55%) | Memory: 11.5 MB (6.28%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -5,20 +5,28 @@ public:
         if(vis[a][b]) return false;
         vis[a][b] = 1;
 
+        //b ka waise ka waisa, x ko pura bhar do
         if(dfs(xm,b,xm,ym,target,vis)) return true;
 
+        //a ka waise ka waisa, y ko pura bhar do
         if(dfs(a,ym,xm,ym,target,vis)) return true;
 
+        //a ko khali b waise ka waisa
         if(dfs(0,b,xm,ym,target,vis)) return true;
 
+        //b ko khali a ko waise ka waisa
         if(dfs(a,0,xm,ym,target,vis)) return true;
 
+
+        //a me se nikal ke b me dal
         int pour = min(a,ym-b);
         if(dfs(a-pour,b+pour,xm,ym,target,vis)) return true;
 
+        //b mese nikal ke a me dal
         pour = min(b,xm-a);
         if(dfs(a+pour,b-pour,xm,ym,target,vis)) return true;
 
+        // saw all combination didn't found any valid one
         return false;
     }
     bool canMeasureWater(int x, int y, int target) {
@@ -26,3 +34,17 @@ public:
         return dfs(0,0,x,y,target,vis);
     }
 };
+
+/*
+
+mathematical approach
+
+agar dono ka i.e gcd(x,y) fully divide kr skta hai apne target ko toh ans possible hai
+
+edge case: 
+    agar x+y > target; so anyhow not possible 
+
+    if x + y > target:
+        return False
+    return target%__gcd(x,y) == 0
+*/
