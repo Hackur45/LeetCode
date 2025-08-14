9eac0a00bf0f0a39245e9485bee57b9794910ced
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-08-14 20:36:47 +0530

    Time: 4 ms (27.33%) | Memory: 10.8 MB (10.80%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     string finalString(string s) {
+        int i = 0;
         string ans = "";
-
-        for(char a : s){
+        for(char &a : s){
             if(a == 'i'){
-                reverse(ans.begin(),ans.end());
+                i++;
+            }else if(i%2 == 0){
+                ans = ans + a;
             }else{
-                ans =  ans + a;
+                ans = a + ans;
             }
         }
-
+        if(i % 2 == 1){
+            reverse(ans.begin(),ans.end());
+        }
         return ans;
     }
 };
