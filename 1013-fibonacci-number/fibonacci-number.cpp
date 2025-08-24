d9464a0bd6455e8606dd363ed7c9d4afee54d363
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-08-24 20:18:27 +0530

    Time: 2475 ms (22.16%) | Memory: 364.7 MB (11.63%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,17 @@
-const int N = 1e5+10;
 class Solution {
 public:
-
-//Dynamic Programming approach
-
-    int dp[N];
-
-    Solution() { 
-        fill(dp, dp + N, -1); 
-    }
-
-    int fib(int n) {
+    int ans(int n, vector<int> &dp){
         if(n == 0) return 0;
         if(n == 1) return 1;
-        if(dp[n] != -1) return dp[n];
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
 
         return dp[n] = fib(n-1) + fib(n-2);
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return ans(n,dp);
     }
 };
