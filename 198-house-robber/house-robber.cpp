274797aa085381be0e9caae6b28cdcf7923dcfd2
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-08-28 14:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.70%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,21 @@
-class Solution {
+class Solution {    
 public:
+    int dfs(vector<int> &dp, vector<int> &nums,int n){
+        if(n == 0) return nums[0];
+
+        if(n < 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+
+        int take = nums[n] + dfs(dp,nums,n-2);
+        int not_take = dfs(dp,nums,n-1) + 0;
+
+        return dp[n] = max(take,not_take);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
 
-        dp[0] = nums[0];
-
-        for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i > 1) take += dp[i-2];
-            int not_take = dp[i-1];
-
-            dp[i] = max(take,not_take); 
-        }
-
-        return dp[n-1];
+        return dfs(dp,nums,n-1);
     }
 };
