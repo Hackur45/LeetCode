c99f34fc64060dcfb2fd0239dc04f52352a48d81
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-09-07 07:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (82.44%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& t) {
         int n = t.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<int> prev(n,0);
 
         for(int i=0;i<n;i++){
-            dp[n-1][i] = t[n-1][i];
+            prev[i] = t[n-1][i];
         }
-
+        vector<int> curr(n,0);
         for(int i = n-2; i>= 0; i--){
             for(int j=0;j<=i;j++){
-                dp[i][j] = min(dp[i+1][j]+t[i][j],dp[i+1][j+1] +t[i][j]);
+                curr[j] = min(prev[j]+t[i][j],prev[j+1] +t[i][j]);
             }
+            prev = curr;
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 };
