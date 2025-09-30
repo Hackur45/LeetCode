b98e61315f145323860f425aca5c7fa9293b7df1
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-09-30 12:28:49 +0530

    Time: 409 ms (61.14%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,4 @@
-SELECT MAX(num) AS num
-FROM (
-    SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(*) = 1
-) AS SingleNumbers;
+# Write your MySQL query statement below
+select max(num) as num from (
+    select num from MyNumbers group by num having count(num) = 1
+) as temp;
