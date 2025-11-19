1eaa6d62f562208c47f76ebbc505be81fbb507ed
Mandar Gurjar <135321556+Hackur45@users.noreply.github.com>
2025-11-19 13:18:12 +0530

    Time: 317 ms (75.95%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,15 @@
-# Write your MySQL query statement below
-with getmgid as(
-    select managerId as id from employee
-    group by managerid
-    having count(managerId) >= 5
-)
+-- # Write your MySQL query statement below
+-- with getmgid as(
+--     select managerId as id from employee
+--     group by managerid
+--     having count(managerId) >= 5
+-- )
+-- select a.name
+-- from employee a join getmgid b
+-- on a.id = b.id
+
+
 select a.name
-from employee a join getmgid b
-on a.id = b.id
+from employee a join 
+(select b.managerId as id from employee b group by managerId having count(managerId) >= 5) as c
+on a.id = c.id
